dc9276c2bdb022e4d9e8c609a17007d7f23d648a
George Nayfonov <naifonov.gk@gmail.com>
2024-05-31 12:30:38 -0400

    feat: adds 404 page

--- src/app/layouts/BaseLayout/BaseLayout.tsx
+++ src/app/layouts/BaseLayout/BaseLayout.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 
+import clsx from 'clsx';
+
 import { Footer } from '@/widgets/Footer';
 import { Header } from '@/widgets/Header';
 
 import styles from './BaseLayout.module.scss';
 
 type BaseLayoutProps = {
+  hasFooter?: boolean;
   children: React.ReactNode;
 };
 
-export const BaseLayout: React.FC<BaseLayoutProps> = ({ children }) => {
+export const BaseLayout: React.FC<BaseLayoutProps> = ({ hasFooter = true, children }) => {
   return (
     <React.Fragment>
       <Header className={styles.header} />
-      <main className={styles.main}>
+      <main className={clsx(styles.main, styles.noFooter)}>
         <div className={styles.mainContent}>{children}</div>
       </main>
-      <Footer className={styles.footer} />
+      {hasFooter && <Footer className={styles.footer} />}
     </React.Fragment>
   );
 };
