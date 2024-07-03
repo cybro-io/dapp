bdc91a7fe4596ad9bdc64751aa33de2f24aca72d
George Nayfonov <naifonov.gk@gmail.com>
2024-07-03 13:26:10 -0400

    feat: baseUrl from env

--- orval.config.ts
+++ orval.config.ts
@@ -1,12 +1,17 @@
+import dotenv from 'dotenv';
 import { defineConfig } from 'orval';
+
+dotenv.config({ path: '.env.local' });
+
+const baseUrl = process.env.NEXT_PUBLIC_API_URL as string;
 
 export default defineConfig({
   dapp: {
-    input: 'https://testnet-dapp-api.cybro.io/openapi.json',
+    input: `${baseUrl}/openapi.json`,
     output: {
       target: './src/shared/types/__generated/api',
       client: 'react-query',
-      baseUrl: 'https://testnet-dapp-api.cybro.io',
+      baseUrl,
     },
   },
 });
