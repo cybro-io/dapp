8d1c5328a0b9ee9a78324b91b0d3f4d269037073
Aleksey Dementev <devDema@bwg-io.site>
2024-09-15 20:54:28 +0300

    fix: withdraw approve

--- src/shared/hooks/vault/useWithdraw.ts
+++ src/shared/hooks/vault/useWithdraw.ts
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { useWeb3ModalAccount } from '@/shared/lib';
-import { utils } from 'ethers';
+import { MaxUint256 } from '@ethersproject/constants';
+import { useWeb3ModalProvider } from '@web3modal/ethers5/react';
+import { BigNumber, ethers, utils } from 'ethers';
 
 import { useEthers } from '@/app/providers';
 import { Mixpanel, MixpanelEvent } from '@/shared/analytics';
 import { useToast } from '@/shared/hooks';
+import { useWeb3ModalAccount } from '@/shared/lib';
 import {
   Nullable,
   Token,
@@ -33,11 +35,19 @@ export const useWithdraw = (
   const { address, isConnected } = useWeb3ModalAccount();
   const { mutate } = useAddVaultActionApiV1VaultsVaultIdActionPost();
   const { tokens } = useEthers();
+  const { walletProvider } = useWeb3ModalProvider();
 
   const withdraw = React.useCallback(
     async (amount: string) => {
       const vaultAddress = vaultContract?.address;
-      if (!tokenContract || !vaultAddress || !vaultContract || !isConnected || !address) {
+      if (
+        !tokenContract ||
+        !vaultAddress ||
+        !vaultContract ||
+        !isConnected ||
+        !address ||
+        !walletProvider
+      ) {
         triggerToast({
           message: `Something went wrong`,
           description:
@@ -52,14 +62,23 @@ export const useWithdraw = (
         const decimals = await tokenContract.decimals();
         const weiAmount = utils.parseUnits(amount, decimals);
 
-        const withdrawEstimatedGas = await vaultContract.estimateGas.redeem(
-          weiAmount,
-          address,
-          address,
-        );
+        const provider = new ethers.providers.Web3Provider(walletProvider);
+        const signer = provider.getSigner();
+
+        const vault = new ethers.Contract(vaultAddress, vaultContract.interface, signer) as Vault;
+
+        const allowance = (await vault.allowance(address, address)) as BigNumber;
+
+        if (allowance.lt(weiAmount)) {
+          const approveTx = await vault.approve(address, MaxUint256);
+          setButtonMessage('Approving...');
+          await approveTx.wait();
+        }
+
+        const withdrawEstimatedGas = await vault.estimateGas.redeem(weiAmount, address, address);
         const gasLimit = increaseGasLimit(withdrawEstimatedGas, 1.2);
 
-        const withdrawTx = await vaultContract.redeem(weiAmount, address, address, { gasLimit });
+        const withdrawTx = await vault.redeem(weiAmount, address, address, { gasLimit });
         setButtonMessage('Redeeming...');
         await withdrawTx.wait();
 
